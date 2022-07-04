921ea54af439a4ee5f15f7501f20a1da0858e2d0
Rinil Parmar <86836620+Rinil-Parmar@users.noreply.github.com>
2022-07-04 22:33:25 +0530

    Add files via upload

---
+++ src/main/java/com/software/base/AccountingProjectSem4Application.java
@@ -0,0 +1,13 @@
+package com.software.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProjectSem4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingProjectSem4Application.class, args);
+	}
+
+}
